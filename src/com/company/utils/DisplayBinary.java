61fcb2950ac62359759911a5ba0ba1c4d4fe8daa
rflorian2017 <roby_florian@yahoo.com>
2019-01-12 09:45:33 +0200

    Dipslay Binary

---
+++ src/com/company/utils/DisplayBinary.java
@@ -0,0 +1,37 @@
+package com.company.utils;
+
+public class DisplayBinary {
+
+    //TODO Pteruta
+    /**
+     * Accepts a number as param and displays a line corresponding to the binary number
+     * Ex: 5
+     * X
+     * O
+     * X
+     * @param number
+     * @return
+     */
+    public static String displayBinaryDecimals(int number) {
+        String toReturn = "";
+        return toReturn;
+
+    }
+
+    //TODO : Alin M
+    /**
+     * Accepts a number as param and displays a line corresponding to the binary number
+     * Ex: 5
+     * X
+     * O
+     * X
+     * @param number
+     * @return
+     */
+    public static String displayBinaryUnits(int number) {
+        String toReturn = "";
+        return toReturn;
+    }
+
+
+}
