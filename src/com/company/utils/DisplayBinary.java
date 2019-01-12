effc491f22a083150a0978c9a1921b4f55a5e85c
alin <alin.margineanu92@yahoo.co.uk>
2019-01-12 12:47:35 +0200

    corresponding binary units (updated)

--- src/com/company/utils/DisplayBinary.java
+++ src/com/company/utils/DisplayBinary.java
@@ -3,12 +3,14 @@ package com.company.utils;
 public class DisplayBinary {
 
     //TODO Pteruta
+
     /**
      * Accepts a number as param and displays a line corresponding to the binary number
      * Ex: 5
      * X
      * O
      * X
+     *
      * @param number
      * @return
      */
@@ -19,19 +21,97 @@ public class DisplayBinary {
     }
 
     //TODO : Alin M
+
     /**
      * Accepts a number as param and displays a line corresponding to the binary number
      * Ex: 5
      * X
      * O
      * X
+     *
      * @param number
      * @return
      */
     public static String displayBinaryUnits(int number) {
         String toReturn = "";
+
+
+        if (number == 0){
+            return "OOOO";
+        }
+        else (number >= 10){
+            return null;
+        }
+
+        if (number == 1){
+            return "OOOX";
+        }
+        else (number >= 10){
+            return null;
+        }
+
+        if (number == 2){
+            return "OOXO";
+        }
+        else (number >= 10){
+            return null;
+        }
+
+        if (number == 3){
+            return "OOXX";
+        }
+        else (number >= 10){
+            return null;
+        }
+
+        if (number == 4){
+            return "OXOO";
+        }
+        else (number >= 10){
+            return null;
+        }
+
+        if (number == 5){
+            return "OXOX";
+        }
+        else (number >= 10){
+            return null;
+        }
+
+        if (number == 6){
+            return "OXXO";
+        }
+        else (number >= 10){
+            return null;
+        }
+
+        if (number == 7){
+            return "OXXX";
+        }
+        else (number >= 10){
+            return null;
+        }
+        if (number == 8){
+            return "XOOO";
+        }
+        else (number >= 10){
+            return null;
+        }
+        if (number == 9){
+            return "XOOX";
+        }
+        else (number >= 10){
+            return null;
+        }
+
         return toReturn;
     }
 
+    /*
+    private String displayBinaryUnits(0) {
+        String toReturn = "OOOO"
+        return toReturn;
+    }
+    */
 
 }
